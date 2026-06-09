134d9bce7c4102ff5ce06a64a5a7df46c71479ad
timilyo80 <78914833+timilyo80@users.noreply.github.com>
2026-06-09 18:38:39 -0400

    Visual update and working buttons but still WIP

--- CommonAssetsManager.js
+++ CommonAssetsManager.js
@@ -1,11 +1,13 @@
+
+//Some reusable assets from when I thought I wouldn't make a single page website. Still keep it around because why not.
+
 class CommonNavigation extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <nav>
-            <a href="index.html">
-                <img src="Pictures/UnrealWeb.png" alt="Test Link">
-                <button onclick="Unreal()">Click me</button>
-            </a>
+            <div>
+                <img src="Pictures/UnrealWeb.png" alt="My Unreal Engine projects" onclick="ShowProject(0)">
+            </div>
         </nav>
         `
     }
@@ -14,9 +16,11 @@ class CommonNavigation extends HTMLElement {
 class CommonFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-        <p>Website made entirelly by Émile Gélinas and hosted on <a href="https://github.com/timilyo80" title="Go to my Github profile">my Github</a></p>
-        <p>No LLM/AI were used during the creation process of this website</p>
-        <p>You can contact me by email at <a href="mailto:emg0811@gmail.com" title="Go to email program to send me an email">emg0811@gmail.com</a></p>
+
+            <p>- Website made entirelly by Émile Gélinas and hosted on <a href="https://github.com/timilyo80" title="Go to my Github profile">my Github</a></p>
+            <p>- No LLM/AI were used during the creation process of this website</p>
+            <p>- You can contact me by email at <a href="mailto:emg0811@gmail.com" title="Go to email program to send me an email">emg0811@gmail.com</a></p>
+  
         `
     }
 }
--- ListManager.js
+++ ListManager.js
@@ -1,10 +1,15 @@
 
+/*Makeshift database to quickly add more projects later
+0 = Unreal
+1 = Godot
+2 = Doom*/
 const data = `[
     [
         {
             "Title": "Test",
             "Description": "It's good lol",
             "Picture": "CorporateLadder.jpg",
+            "TitlePicture": "CorporateLadder_T.png",
             "Link": "Here's a link"
         },
 
@@ -12,6 +17,7 @@ const data = `[
             "Title": "Furry",
             "Description": "OwO",
             "Picture": "CorporateLadder.jpg",
+            "TitlePicture": "CorporateLadder_T.png",
             "Link": "UwU"
         },
 
@@ -19,6 +25,7 @@ const data = `[
             "Title": "Fury",
             "Description": "èwé",
             "Picture": "CorporateLadder.jpg",
+            "TitlePicture": "CorporateLadder_T.png",
             "Link": "-w-"
         },
 
@@ -26,6 +33,7 @@ const data = `[
             "Title": "Linux",
             "Description": "Foo",
             "Picture": "CorporateLadder.jpg",
+            "TitlePicture": "CorporateLadder_T.png",
             "Link": "Bar"
         },
 
@@ -33,21 +41,8 @@ const data = `[
             "Title": "yyyyyyyyyyyyyyyyyyyy",
             "Description": "eeeeeeeeeeee eeeeeeeeeeeee",
             "Picture": "CorporateLadder.jpg",
+            "TitlePicture": "CorporateLadder_T.png",
             "Link": "ggggggggggggggg ggggggggggggggg"
-        },
-
-        {
-            "Title": "Vieux Placement de Produit",
-            "Description": "YO",
-            "Picture": "CorporateLadder.jpg",
-            "Link": "YOP!!!"
-        },
-
-        {
-            "Title": "zzzzzzzzzzzzzzzz",
-            "Description": "aaaaaaaaaaaaaaa aaaaaaaaaaaaaaa",
-            "Picture": "CorporateLadder.jpg",
-            "Link": "bbbbbbbbbbbbbbb bbbbbbbbbbbbbb"
         }
     ],
     [
@@ -55,6 +50,7 @@ const data = `[
             "Title": "zzzzzzzzzzzzzzzz",
             "Description": "aaaaaaaaaaaaaaa aaaaaaaaaaaaaaa",
             "Picture": "CorporateLadder.jpg",
+            "TitlePicture": "CorporateLadder_T.png",
             "Link": "bbbbbbbbbbbbbbb bbbbbbbbbbbbbb"
         }
     ]
@@ -66,39 +62,51 @@ const mainList = document.getElementById("list");
 let outputString = "";
 let numProject = 0;
 
-function Unreal(){
-    showList.style.display = "flex";
-    showAbout.style.display = "none";
+//Show a specific project when its thumbnail is clicked
+function ShowProject(ProjectIndex){
+    if (typeof ProjectIndex == "number"){
+        numProject = ProjectIndex;
+        LoadProject();
+        showList.style.display = "flex";
+        showAbout.style.display = "none";
+    }
 }
 
-if (dataParsed.length > 0) {
-    let exitLoop = false;
-    let currentProject = dataParsed[numProject];
-    let nInRow = currentProject.length
-    let nOfLoops = 0;
-    let i = 0;
-    let y = 0;
-    do {
-        if (nInRow - 5 > 0) {
-            y = 5;
-            nInRow -= 5;
-        } else {
-            y = nInRow;
-            exitLoop = true;
-        }
-        outputString = `${outputString}<tr>\n`;
-        i = 0;
-        for (i; i < y; i++) {
-            const currentData = dataParsed[numProject][i+nOfLoops];
-            outputString = `${outputString}<td>\n<div>\n<svg width="200" height="60" text-overflow="ellipsis"><text x="100" y="30" fill="white" stroke="black" font-size="20" text-anchor="middle" font-weight="bold">${currentData.Title}</text></svg>\n<img src="Pictures/${currentData.Picture}" alt="Test Link">\n</div>\n</td>\n`;
-        }
-        outputString = `${outputString}</tr>\n`;
-        nOfLoops += 5;
-    } while (!exitLoop);
-    numProject++
+//Get the selected project's information from the database by using the ID stored in numProject
+function LoadProject(){
+    if (dataParsed.length > 0) {
+        let exitLoop = false;
+        let currentProject = dataParsed[numProject];
+        let nInRow = currentProject.length;
+        let nOfLoops = 0;
+        let i = 0;
+        let y = 0;
+        do {
+            if (nInRow - 5 > 0) {
+                y = 5;
+                nInRow -= 5;
+            } else {
+                y = nInRow;
+                exitLoop = true;
+            }
+            outputString = `${outputString}<tr>\n`;
+            i = 0;
+            for (i; i < y; i++) {
+                const currentDataIndex = i + nOfLoops
+                const currentData = dataParsed[numProject][currentDataIndex];
+                //outputString = `${outputString}<td>\n<div>\n<svg width="200" height="60" text-overflow="ellipsis"><text x="100" y="30" fill="white" stroke="black" font-size="20" text-anchor="middle" font-weight="bold">${currentData.Title}</text></svg>\n<img src="Pictures/${currentData.Picture}" alt="Test Link">\n</div>\n</td>\n`;
+                outputString = `${outputString}<td>\n<div>\n<img src="Pictures/${currentData.TitlePicture}" alt="Test Tittle">\n<img class="pointer" src="Pictures/${currentData.Picture}" alt="Test Link" onclick="ShowData(${currentDataIndex})">\n</div>\n</td>\n`;
+            }
+            outputString = `${outputString}</tr>\n`;
+            nOfLoops += 5;
+        } while (!exitLoop);
+        mainList.innerHTML = outputString;
+    }
+}
+
+function ShowData(DataIndex){
+    console.log(DataIndex)
 }
 
 //outputString = `${outputString}<pre>${dataParsed[0].Description}\n${dataParsed[0].Link}`
 //outputString = `${outputString}\n${dataParsed.length}</pre><p>WAAAAAAAAAAAAAAAAAAAAAAAA</p>`
-
-mainList.innerHTML = outputString;
